6f8a0263d3a25b7d3d92b863e802c058f8a06c32
Cheryl Lee <yli.yanchen@gmail.com>
2025-02-10 13:51:04 -0800

    landing page done

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@heroui/react';
+import Image from 'next/image';
+import logo from '@/public/Logo.png';
+
+export const Logo = () => {
+  return (
+    <div>
+      <Image src={logo} alt='Tribe Icon' width={300} className='mt-24 ' />
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Navbar className='h-24 w-full flex p-4 font-bold bg-primary text-white text-xl justify-between'>
+      <NavbarBrand>
+        <Logo />
+      </NavbarBrand>
+      <NavbarContent
+        className='hidden sm:flex gap-4 mx-8 px-4'
+        justify='center'
+      >
+        <NavbarItem>
+          <Link color='foreground' href='#publicout-reach' className='mx-2'>
+            Public Outreach
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link aria-current='page' href='#project-status' className='mx-2'>
+            Project Status
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color='foreground'
+            href='#documentation-and-reference'
+            className='mx-2'
+          >
+            Documentations & Resources
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color='foreground' href='#contact-us' className='mx-2'>
+            Contact Us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      {/* <NavbarContent justify='end'>
+        <NavbarItem className='hidden lg:flex'>
+          <Link href='#'>Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color='primary' href='#' variant='flat'>
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent> */}
+    </Navbar>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+import Image from 'next/image';
+import background from '@/public/LandingPage.jpg';
+import badge from '@/public/badge.jpg';
+import lefterrow from '@/public/left-arrow.jpg';
+import righterror from '@/public/right-arrow.jpg';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='flex flex-col items-center'>
+      <div className='relative w-full h-screen flex flex-col items-center justify-center'>
+        {/* Background Image */}
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={background}
+          alt='Home page background image'
+          fill
+          className='z-0'
+          style={{
+            opacity: 0.15,
+            objectFit: 'cover',
+          }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {/* Content */}
+        <div className='z-10 flex flex-col items-center justify-center text-center px-24'>
+          <h1 className='font-extrabold text-5xl'>
+            Paskenta Active Transportation Plan
+          </h1>
+          <p className='font-semibold text-lg mt-4'>
+            Our goal is to achieve a 50% reduction in pedestrian and cyclist
+            fatalities and serious injuries by 2030 by prioritizing equitable
+            infrastructure upgrades, traffic calming measures, and safety
+            education, with a focus on high-risk corridors and underserved
+            communities.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/* Badge Image */}
+      <div className='flex items-center justify-center'>
+        <Image src={badge} alt='badge in landing page' className='z-0' />
+      </div>
+
+      {/* Mission Section */}
+      <div className='flex flex-col items-center justify-center my-12'>
+        <div className='flex flex-row size-16 items-center justify-center mb-8'>
+          <Image src={lefterrow} alt='left arrow' className='z-0 mr-6' />
+          <h1 className='font-extrabold text-3xl mx-6'>Mission</h1>
+          <Image src={righterror} alt='right arrow' className='z-0' />
+        </div>
+        <div className='px-24'>
+          <p>
+            The Paskenta Band of Nomlaki Indians is committed to fostering a
+            safe, sustainable, and connected community through an equitable and
+            culturally respectful active transportation plan. By prioritizing
+            the health, well-being, and mobility of our tribal members and
+            neighbors, we aim to create accessible pathways, promote walking and
+            cycling, and honor our ancestral lands while reducing our
+            environmental impact. Together, we will build a future where every
+            journey reflects our values of stewardship, unity, and resilience.
+          </p>
+          <br className='size-6' />
+          <p className='my-2'>Key Elements:</p>
+          <ul className='list-disc list-inside space-y-2'>
+            <li>
+              <span className='font-bold'>Cultural Respect: </span>Honors the
+              tribe's heritage and connection to the land.{' '}
+            </li>
+            <li>
+              <span className='font-bold'>Equity and Accessibility: </span>
+              Ensures all community members, including elders and youth, have
+              safe and accessible transportation options.
+            </li>
+            <li>
+              <span className='font-bold'>Sustainability:</span> Reduces
+              environmental impact and promotes healthy lifestyles.{' '}
+            </li>
+            <li>
+              <span className='font-bold'>Community Focus: </span>Prioritizes
+              the needs of tribal members and neighboring communities.{' '}
+            </li>{' '}
+            <li>
+              <span className='font-bold'>Safety and Connectivity: </span>Builds
+              infrastructure that connects people to key destinations safely.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
